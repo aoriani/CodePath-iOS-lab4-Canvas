74f417a5f11e38767c798a0043e030c0ebf212b1
Andre Oriani <aoriani@gmail.com>
2016-02-24 19:12:01 -0800

    Initial Commit

---
+++ SmileyFaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmileyFaces
+//
+//  Created by Andre Oriani on 2/24/16.
+//  Copyright © 2016 Orion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
